1962a0384f3c3315b240767f119f50b249d79721
Dimitri Couwenberg <couwenberg@gmail.com>
2016-02-13 22:19:38 +1100

    task minification added

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,6 +10,20 @@ module.exports = function(grunt) {
 
                 ]
             }
+        },
+        jshint: {
+            beforeconcat: ['client/app/**/*.js'],
+            afterconcat: ['generated/js/total.js']
+        },
+        uglify: {
+            options: {
+                mangle: false
+            },
+            my_target: {
+                files: {
+                    'generated/js/total.min.js': ['generated/js/total.js']
+                }
+            }
         }
 
     }
@@ -20,11 +34,13 @@ module.exports = function(grunt) {
     // load node packages that are grunt plugins
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
     // Note for the reader: concatentation is an alternative to require.js or common.js
     // concatentation is for small to medium stuff...be pragmatic.
 
     // a default task (so you don't have to specify one at the CLI); watch should be last.
-    grunt.registerTask('default', ['concat','jshint']);
+    grunt.registerTask('default', ['concat','uglify']);
 
     //TODO: Unit tests, JS LINT, other.z
 };
